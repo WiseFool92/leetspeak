8635a1c29f5a8a226a15fa1814e5cf5c8fb30777
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-05-11 13:33:49 -0700

    add code that passes e to 3 test

--- LeetSpeak/Models/LeetConverter.cs
+++ LeetSpeak/Models/LeetConverter.cs
@@ -4,7 +4,18 @@ namespace LeetSpeak
   {
     public string LeetSpeakConverter(string userEntry)
     {
-      return "false";
+      char[] charArray = userEntry.ToCharArray();
+
+      for (int i = 0; i < charArray.Length; i++)
+      {
+        if (charArray[i] == 'e' || charArray[i] == 'E')
+        {
+          charArray[i] = '3';
+        }
+      }
+
+      string transformedString = string.Join("", charArray);
+      return transformedString;
     }
     public static void Main()
     {
